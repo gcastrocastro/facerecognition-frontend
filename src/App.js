3b69965d53b9a975107a55536fa7f9d2a2814645
gcastrocastro <gcastrocastro.25@gmail.com>
2023-01-07 19:27:34 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -57,8 +57,8 @@ class App extends Component {
     this.setState({route: route});
   }
 
-  calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  calculateFaceLocation = (response) => {
+    const clarifaiFace = response.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
