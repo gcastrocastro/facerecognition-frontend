5e52167e0a28cb6895b3fd3d3827a3f9a0cf4875
gcastrocastro <gcastrocastro.25@gmail.com>
2023-01-07 21:12:38 -0700

    reverted changes

--- src/App.js
+++ src/App.js
@@ -58,31 +58,17 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = JSON.parse(data, null, 2).outputs[0].data.regions[0]
-     .region_info.bounding_box;
-    const image = document.getElementById("inputimage");
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    return {
+    return  {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
-
-  // calculateFaceLocation = (data) => {
-  //   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-  //   const image = document.getElementById('inputimage');
-  //   const width = Number(image.width);
-  //   const height = Number(image.height);
-  //   return  {
-  //     leftCol: clarifaiFace.left_col * width,
-  //     topRow: clarifaiFace.top_row * height,
-  //     rightCol: width - (clarifaiFace.right_col * width),
-  //     bottomRow: height - (clarifaiFace.bottom_row * height)
-  //   }
-  // }
   
 
   displayFaceBox = (box) => {
@@ -94,98 +80,34 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    const raw = JSON.stringify({
-      user_app_id : {
-        user_id: "aqtpwt25qqga",
-        app_id: "facerecognition-react"
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: this.state.input
-            },
-          },
-        },
-      ],
-    });
- 
-    fetch(
-       "https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/outputs",
-       {
-         method: "POST",
-         headers: {
-           Accept: "application/json",
-           Authorization: "acdcaf9db49947e7b4f26850ff286c25",
-         },
-         body: raw,
-       }
-     )
-     .then((response) => response.text())
-     .then((result) => this.displayFaceBox(this.calculateFaceLocation(result)))
-     .catch((error) => console.log("error", error));
-  }
-
-  // onButtonSubmit = () => {
-  //   this.setState({imageUrl: this.state.input});
-  //             fetch('https://facerecognition-api-fzim.onrender.com/imageurl', {
-  //               method: 'post',
-  //               headers: {'Content-Type': 'application/json'},
-  //               body: JSON.stringify({
-  //                 input: this.state.input
-  //             })
-  //           })
-  //             .then(response => response.json())
-  //             .then(response => {
-  //               if (response) {
-  //                 fetch('https://facerecognition-api-fzim.onrender.com/image', {
-  //                   method: 'put',
-  //                   headers: {'Content-Type': 'application/json'},
-  //                   body: JSON.stringify({
-  //                     id: this.state.user.id
-  //                   })
-  //                 })
-  //                   .then(response => response.json())
-  //                   .then(count => {
-  //                     this.setState(Object.assign(this.state.user, { entries: count }))
-  //                   })
-  //                   .catch(console.log)
-  //             }
-  //           this.displayFaceBox(this.calculateFaceLocation(response))
-  //         })
-  //         .catch(err => console.log('woops'));
-  //   }
-    
-
-  // const raw = JSON.stringify({
-  //   "user_app_id": {
-  //     "user_id": "aqtpwt25qqga",
-  //     "app_id": "7e8ceccac1124a85aa77dc54ff12b349"
-  //   },
-  //   "inputs": [
-  //     {
-  //       "data": {
-  //         "image": {
-  //           "url": "https://samples.clarifai.com/metro-north.jpg"
-  //         }
-  //       }
-  //     }
-  //   ]
-  // });
-  
-  // const requestOptions = {
-  //   method: 'POST',
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Authorization': 'Key acdcaf9db49947e7b4f26850ff286c25'
-  //   },
-  //   body: raw
-  // };
-  
-  // fetch("https://api.clarifai.com/v2/models/{a403429f2ddf4b49b307e318f00e528b}/versions/{2.9.1}/outputs", requestOptions)
-  //   .then(response => response.text())
-  //   .then(result => console.log(JSON.parse(result, null, 2).outputs[0].data))
-  //   .catch(error => console.log('error', error));
+    this.setState({imageUrl: this.state.input});
+              fetch('https://facerecognition-api-fzim.onrender.com/imageurl', {
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                  input: this.state.input
+              })
+            })
+              .then(response => response.json())
+              .then(response => {
+                if (response) {
+                  fetch('https://facerecognition-api-fzim.onrender.com/image', {
+                    method: 'put',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                      id: this.state.user.id
+                    })
+                  })
+                    .then(response => response.json())
+                    .then(count => {
+                      this.setState(Object.assign(this.state.user, { entries: count }))
+                    })
+                    .catch(console.log)
+              }
+            this.displayFaceBox(this.calculateFaceLocation(response))
+          })
+          .catch(err => console.log(err));
+    }
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
@@ -215,41 +137,6 @@ class App extends Component {
       </div>
     );
   }
-
-//   render() { 
-//     return (
-//     <div className="App">
-//       <ParticlesBg type="circle" bg={true} />
-//       <Navigation 
-//           onRouteChange={this.onRouteChange}
-//           isSignedIn={this.state.isSignedIn}/>
-//       { this.state.route === 'home' 
-//         ? 
-//           <div>
-//               <Logo />
-//               <Rank 
-//                   name={this.state.user.name}
-//                   entries={this.state.user.entries}
-//               />
-//               <ImageLinkForm 
-//                   onInputChange={this.onInputChange} 
-//                   onButtonSubmit={this.onButtonSubmit}
-//               />
-//               <FaceRecognition
-//                   box={this.state.box} imageUrl={this.state.imageUrl}
-//               />
-//           </div>
-//         : (
-//           this.state.route === 'signin' 
-//           ?
-//           <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-//           : 
-//           <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-//           )
-//         }
-//     </div>
-//   );
-// }
 }
 
 export default App;
